63013d5f145896c7a777f1219e494ceb24f87774
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-08-26 00:34:54 +0500

    fix: убрать атрибут children

--- src/components/App.js
+++ src/components/App.js
@@ -52,40 +52,33 @@ function App() {
           name="edit-profile"
           title="Редактировать профиль"
           id="profileFormValidator"
-          children={
-              <>
-                <input name="user_name" id="user_name" className="form__text form__text_edit_name" type="text" placeholder="Имя" requiredminlength="2" maxLength="40" />
-                <span id="user_name-error" className="form__input-error"></span>
-                <input name="activity" id="activity" className="form__text form__text_edit_about-me" type="text" placeholder="Вид деятельности" requiredminlength="2" maxLength="200" />
-                <span id="activity-error" className="form__input-error"></span>
-              </>
-            }
-          />
+          >
+            <input name="user_name" id="user_name" className="form__text form__text_edit_name" type="text" placeholder="Имя" requiredminlength="2" maxLength="40" />
+            <span id="user_name-error" className="form__input-error"></span>
+            <input name="activity" id="activity" className="form__text form__text_edit_about-me" type="text" placeholder="Вид деятельности" requiredminlength="2" maxLength="200" />
+            <span id="activity-error" className="form__input-error"></span>
+          </PopupWithForm>
       <PopupWithForm
           isOpen = {isAddPlacePopupOpen ? "popup_is-opened" : ""}
           onClose = {closeAllPopups}
           name="add-card"
           title="Новое место"
           id="cardFormValidator"
-          children={
-            <>
-              <input name="image_name" id="image_name" className="form__text form__text_edit_name" type="text" placeholder="Название" requiredminlength="2" maxLength="30" />
-              <span id="image_name-error" className="form__input-error"></span>
-              <input name="url_image" id="url_image" className="form__text form__text_edit_about-me" type="url" placeholder="Ссылка на картинку" required />
-              <span id="url_image-error" className="form__input-error"></span>
-            </>
-          } />
+          >
+            <input name="image_name" id="image_name" className="form__text form__text_edit_name" type="text" placeholder="Название" requiredminlength="2" maxLength="30" />
+            <span id="image_name-error" className="form__input-error"></span>
+            <input name="url_image" id="url_image" className="form__text form__text_edit_about-me" type="url" placeholder="Ссылка на картинку" required />
+            <span id="url_image-error" className="form__input-error"></span>
+          </PopupWithForm>
       <PopupWithForm
           isOpen = {isEditAvatarPopupOpen ? "popup_is-opened" : ""}
           onClose = {closeAllPopups}
           name="edit-avatar"
           title="Обновить аватар"
-          children={
-            <>
-              <input name="url_avatar" id="url_avatar" className="form__text form__text_edit_about-me" type="url" placeholder="Ссылка на аватарку" required />
-              <span id="url_avatar-error" className="form__input-error"></span>
-            </>
-          } />
+          >
+            <input name="url_avatar" id="url_avatar" className="form__text form__text_edit_about-me" type="url" placeholder="Ссылка на аватарку" required />
+            <span id="url_avatar-error" className="form__input-error"></span>
+          </PopupWithForm>
       <PopupWithForm
           name="delete-card"
           title="Вы уверены?" />
